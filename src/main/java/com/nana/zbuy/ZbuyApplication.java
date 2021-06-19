820e8f4aec071b64adb1837fccb3bc712df4c578
ZB <zb@qq.com>
2021-06-19 21:58:32 +0800

    Feature:基础代码提交

---
+++ src/main/java/com/nana/zbuy/ZbuyApplication.java
@@ -0,0 +1,13 @@
+package com.nana.zbuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZbuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nana/zbuy/ZbuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nana.zbuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZbuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
